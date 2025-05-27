c8092c1df2588fde2330288b41e7749a7284b771
lukmanasaputra <squadwts2@gmail.com>
2025-05-27 14:34:51 +0700

    test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const path = require("path");
-const db = require("./Database");
 const mainRoutes = require("./App/routes");
 
 const port = process.env.PORT || 8080;
