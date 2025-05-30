2dfd94a3d6607185e4558416c4d19674cf5d38e2
lukmanasaputra <squadwts2@gmail.com>
2025-05-31 05:53:01 +0700

    final

--- App/routes/products/index.js
+++ App/routes/products/index.js
@@ -14,42 +14,30 @@ router.get("/", async (req, res) => {
   const sort = req.query.sort || "newest";
   const category = req.query.category || null;
 
-  const [products, categories] = await Promise.all([
+  const [productsResult, categoriesResult] = await Promise.all([
     db.getAllProducts(sort, category),
     db.getAllCategories(),
   ]);
 
+  const now = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
+  );
+
+  const products = productsResult.data.map((product) => {
+    const start = new Date(product.discount_start);
+    const end = new Date(product.discount_end);
+    product.is_discount_active =
+      product.is_discount && now >= start && now <= end;
+    return product;
+  });
+
   res.render("products", {
-    products: products.data,
-    categories: categories.data,
+    products,
+    categories: categoriesResult.data,
     sort,
     selectedCategory: category,
     active: "product",
   });
 });
-
-// router.get("/search", async (req, res) => {
-//   const query = req.query.q || "";
-//   const sort = req.query.sort || "newest";
-//   const category = req.query.category || null;
-
-//   const result = await db.getAllProducts(sort, category);
-//   if (!result.success) return res.send("Gagal memuat produk");
-
-//   let filtered = result.data;
-
-//   // Jika ada query pencarian
-//   if (query) {
-//     filtered = filtered.filter((p) =>
-//       p.name.toLowerCase().includes(query.toLowerCase())
-//     );
-//   }
-
-//   // Render partial produk saja (tanpa layout)
-//   res.render("components/product-grid", {
-//     products: filtered,
-//     layout: false,
-//   });
-// });
 
 module.exports = router;
